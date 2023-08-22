08d0b777a8895b9c8136c7c9c5ac9db94b841326
facamesi52 <facamesi52@gmail.com>
2023-08-22 18:19:58 -0500

    build: Corrections - structure for CreateHotel

--- src/api/hotel/hotel.controller.ts
+++ src/api/hotel/hotel.controller.ts
@@ -3,13 +3,21 @@ import { Request, Response } from "express";
 import { createHotel } from "./hotel.service";
 import { Hotel, CreateHotelData } from "./hotel.types";
 
+function errorHandler(exception: unknown) {
+    const message = (typeof exception === 'string') ? exception.toUpperCase()
+      : (exception instanceof Error) ? exception.message : 'Something was wrong';
+  
+    return message;
+  }
 export async function createHotelController(req: Request, res: Response){
     try{
         const hotelData: CreateHotelData = req.body; 
         const createdHotel: Hotel = await createHotel(hotelData);
         res.status(201).json({ message: 'hotel has been created successfully',createdHotel});
-    } catch ({ message }: any) {
-
+    } catch (error: unknown) {
+        const message = errorHandler(error);
         res.status(400).json({ message })
     }
 }
+
+export default errorHandler;
--- src/api/hotel/index.ts
+++ src/api/hotel/index.ts
@@ -4,7 +4,6 @@ import { createHotelController } from "./hotel.controller";
 
 const router = Router();
 
-// /api/hotels -> POST
 router.post('/', createHotelController);
 
 export default router;
