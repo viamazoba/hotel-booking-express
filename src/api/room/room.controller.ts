9d7550bfecd95365924052f311c1390e698d6137
NataliaChavarria <nataliachm97@gmail.com>
2023-08-27 13:45:14 -0500

    feat: creating room CRUD

--- src/api/room/room.controller.ts
+++ src/api/room/room.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 
 import { createRoom } from "./room.service";
+import { getRooms } from "./room.service";
+import { getRoomById } from "./room.service";
+import { updateRoom } from "./room.service";
+import { deleteRoom } from "./room.service";
 import { Room, CreateRoomData } from "./room.types";
+import { AuthRequest } from "../../auth/auth.types";
 
 function errorHandler(exception: unknown) {
     const message = (typeof exception === 'string') ? exception.toUpperCase()
@@ -18,6 +23,47 @@ export async function createRoomHandler(req: Request, res: Response){
         const message = errorHandler(error);
         res.status(400).json({ message })
     }
+}
+export async function getRoomController (req:Request, res: Response) {
+  try{
+      const rooms = await getRooms();
+      res.status(200).json(rooms);
+  } catch(error:any){
+      const message = errorHandler(error);
+      res.status(500).json({ message})
+
+  }
+}
+export async function getRoomByIdController(req:Request, res: Response) {
+const { id } = req.params;
+try {
+  const room = await getRoomById(id);
+  res.status(200).json(room);
+} catch (error:any) {
+  const message = errorHandler(error);
+  res.status(500).json({ message })
+}    
+}
+export async function updateRoomController(req:AuthRequest, res: Response) {
+  const { id } = req.params;
+  const roomData = req.body;
+  try{
+      const updatedRoom = await updateRoom(id, roomData);
+      res.status(200).json(updateRoom);
+} catch (error:any){
+  const message = errorHandler(error);
+  res.status(500).json({ message })
+}    
+}
+export async function deleteRoomController(req:AuthRequest, res: Response) {
+const { id } = req.params;
+try {
+  const deletedRoom = await deleteRoom(id);
+  res.status(200).json(deletedRoom);
+} catch (error:any) {
+  const message = errorHandler(error);
+  res.status(500).json({ message });
+}   
 }
 
 export default errorHandler;
